c3369b8f6e21c2e04330f73f4803410fd219e153
nrudopisov <nrudopisov@neoflex.ru>
2020-12-06 15:20:49 +0400

    Added google cloud storage creating data logic. Need tests.

--- src/main/java/com/ksrate/archive/ArchiveData.java
+++ src/main/java/com/ksrate/archive/ArchiveData.java
@@ -1,9 +1,37 @@
 package com.ksrate.archive;
 
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.*;
 import com.ksrate.data.Statistic;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 
 public class ArchiveData {
     public void push(Statistic statistic) {
+        Storage storage = getStorage();
+        final String bucketName = "ks-archive";
+        final String objectName = "statistic";
+        Bucket bucket = storage.create(BucketInfo.of(bucketName));
+        final byte[] data = statistic.getSource().getBytes(StandardCharsets.UTF_8);
+        bucket.create(objectName, data);
+    }
 
+    private Storage getStorage() {
+        //TODO: add parameter
+        String authJson = null;
+        if (authJson != null) {
+            try (final FileInputStream jsonInputStream = new FileInputStream(authJson)) {
+                return StorageOptions.newBuilder()
+                        .setCredentials(ServiceAccountCredentials.fromStream(jsonInputStream))
+                        .build().getService();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        } else {
+            return StorageOptions.getDefaultInstance().getService();
+        }
     }
 }
