8193d1f9aa09bb179922d8d3e323e33f26e3335d
Roman1104 <malakhov1104@gmail.com>
2020-12-12 00:09:02 +0500

    Update createSparkUser.sql

--- sql/createSparkUser.sql
+++ sql/createSparkUser.sql
@@ -1,5 +1,5 @@
 CREATE USER ''@''  # to be set in 'username'@'user hostname' format
-  IDENTIFIED BY '';
+  IDENTIFIED BY '';  # user password
 GRANT SELECT,INSERT,UPDATE,DELETE, ALTER
   ON metrics.*
   TO ''@'';  # to be set in 'username'@'user hostname' format
